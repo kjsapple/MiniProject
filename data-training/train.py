6649b023ea8f574e8c0401a6020cf956b157ac19
kjsapple <21eg106a57@anurag.edu.in>
2024-09-29 15:24:43 +0530

    plot

--- data-training/train.py
+++ data-training/train.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, classification_report
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
 import joblib
 
 data=pd.read_csv('data-training/emotions.csv')
@@ -10,6 +11,14 @@ data=pd.read_csv('data-training/emotions.csv')
 x=data.drop('label',axis=1)
 y=data['label']
 
+"""
+sample = data.loc[0, 'fft_0_b':'fft_150_b']
+
+plt.figure(figsize=(16, 10))
+plt.plot(range(len(sample)), sample)
+plt.show()
+
+"""
 y=y.replace('NEGATIVE',0)
 y=y.replace('NEUTRAL',1)
 y=y.replace('POSITIVE',2)
@@ -30,6 +39,8 @@ pred_rf=rf.predict(x_test)
 
 print('Accuracy of Decision Tree:\t\t',accuracy_score(y_pred_dt,y_test)*100)
 print('Accuracy of Random Forest:\t\t',accuracy_score(pred_rf,y_test)*100)
+print()
+print(classification_report(pred_rf,y_test))
 
 
 joblib.dump(rf, 'model.pkl')
